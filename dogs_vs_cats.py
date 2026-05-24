074c2f828543fa89513b3f4c4fb1dc5262a66b25
Yash Gupta <yhgupta0408@gmail.com>
2026-05-24 17:42:22 +0530

    Minor Change

---
+++ dogs_vs_cats.py
@@ -0,0 +1,19 @@
+from fastai.vision.all import *
+import gradio as gr
+
+def is_cat(x): return x[0].isupper()
+
+learn = load_learner('model.pkl')
+
+categories = ('Dog', 'Cat')
+
+def classify_image(img):
+    pred,idx,probs = learn.predict(img)
+    return dict(zip(categories, map(float,probs)))
+
+image = gr.Image()
+label = gr.Label()
+examples = ['dog.jpg', 'cat.jpg', 'dunno.jpg']
+
+intf = gr.Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
+intf.launch(inline=False)
