7df306d6749f7055d4ea2577b9e4ca371b41a8b9
Yash Gupta <yhgupta0408@gmail.com>
2026-06-01 18:59:34 +0530

    Changes

--- micrograd.py
+++ micrograd.py
@@ -1,16 +1,9 @@
 import math
+import random
 import numpy as np
 import matplotlib.pyplot as plt
+from graphviz import Digraph
 
-def f(x):
-    return 3*x**2 - 4*x + 5
-
-f(3.0)
-
-xs = np.arange(-5, 5, 0.25)
-ys = f(xs)
-# plt.plot(xs,ys)
-# plt.show()
 
 class Value:
 
@@ -18,6 +11,7 @@ class Value:
         self.data = data
         self.grad = 0.0
         self._prev = set(_children)
+        self._backward = lambda: None
         self._op = _op
         self.label = label
 
@@ -25,27 +19,85 @@ class Value:
         return f"Value (data = {self.data})"
 
     def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), '+')
+
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+        out._backward = _backward
         return out
     
     def __mul__(self, other):
-        return Value(self.data*other.data, (self, other), '*')
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data*other.data, (self, other), '*')
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = _backward
+        return out
+    
+    def __truediv__(self, other):
+        return self * other ** -1
+    
+    def __neg__(self):
+        return self * -1
+
+    def __sub__(self, other):
+        return self + (-other)
+    
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+        out = Value(self.data**other, (self, ), f'**{other}')
+
+        def _backward():
+            self.grad += other * (self.data ** (other-1)) * out.grad
+        
+        out._backward = _backward
+        return out
+         
+    
+    def __rmul__(self, other):
+        return self * other
+    
+    def __radd__(self, other):
+        return self + other
     
     def tanh(self):
         x = self.data
         t = ((math.exp(2*x)-1)/(math.exp(2*x)+1))
         out = Value(t, (self, ), 'tanh')
+
+        def _backward():
+            self.grad += (1 - t**2)*out.grad
+        out._backward = _backward
         return out
+    
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self, ), 'exp')
 
-a = Value(2.0, label='a')
-b = Value(-3.0, label='b')
-c = Value(10.0, label='c')
-e = a*b; e.label = 'e'
-d = e + c; d.label = 'd'
-f = Value(-2, label='f')
-L = d*f; L.label = 'L'
+        def _backward():
+            self.grad = out.data * out.grad
+        out._backward = _backward
 
-from graphviz import Digraph
+        return out
+    
+    def backward(self):
+        self.grad = 1.0
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+        for node in reversed(topo):
+            node._backward()
+
 
 def trace(root):
     nodes, edges = set(), set()
@@ -78,67 +130,80 @@ def draw_dot(root):
     return dot
 
 
-L.grad = 1.0
-f.grad = 4.0
-d.grad = -2.0
-c.grad = -2.0
-e.grad = -2.0
-a.grad = 6.0
-b.grad = -4.0
-draw_dot(L).render('graph', format='svg', cleanup=True)
-
-
-# plt.plot(np.arange(-5, 5.0, 0.25), np.tanh(np.arange(-5, 5.0, 0.25))); plt.grid(); plt.show()
-
-
-# inputs x1, x2
-x1 = Value(2.0, label='x1')
-x2 = Value(0.0, label='x2')
-w1 = Value(-3.0, label='w1')
-w2 = Value(1.0, label='w2')
-b = Value(6.8813735870195432, label='b')
-x1w1 = x1*w1; x1w1.label = 'x1*w1'
-x2w2 = x2*w2; x2w2.label = 'x2*w2'
-
-x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label = 'x1w1 + x2w2'
-n = x1w1x2w2 + b; n.label = 'n'
-o = n.tanh(); o.label = 'o'
-
-o.grad = 1.0
-n.grad = 1 - o.data**2
-x1w1x2w2.grad = 0.5
-b.grad = 0.5
-x1w1.grad = 0.5
-x2w2.grad = 0.5
-
 draw_dot(o).render('sigmoid', format='svg', cleanup=True)
 
+import torch 
+
+class Neuron:
+
+    def __init__(self, nin):
+        self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
+        self.b = Value(random.uniform(-1, 1))
+
+    def __call__(self, x):
+        act = sum((wi*xi for wi, xi in zip(self.w, x)), self.b) 
+        out = act.tanh()
+        return out
+    
+    def parameters(self):
+        return self.w + [self.b]
 
 
-def lol():
+class Layer:
 
-    h = 0.0001
-    a = Value(2.0, label='a')
-    b = Value(-3.0, label='b')
-    c = Value(10.0, label='c')
-    e = a*b; e.label = 'e'
-    d = e + c; d.label = 'd'
-    f = Value(-2, label='f')
-    L = d*f; L.label = 'L'
-    L1 = L.data
-
-
-    a = Value(2.0+h, label='a')
-    b = Value(-3.0, label='b')
-    c = Value(10.0, label='c')
-    e = a*b; e.label = 'e'
-    d = e + c; d.label = 'd'
-    f = Value(-2, label='f')
-    L = d*f; L.label = 'L'   
-
-    L2 = L.data
-
-    print((L2-L1)/h) 
+    def __init__(self, nin, nout):
+        self.neurons = [Neuron(nin) for _ in range(nout)]
+    
+    def __call__(self, x):
+        outs = [n(x) for n in self.neurons]
+        return outs[0] if len(outs) == 1 else outs
+    
+    def parameters(self):
+        return [p for neuron in self.neurons for p in neuron.parameters()]
 
 
-lol()    
+class MLP:
+
+    def __init__(self, nin, nouts):
+        sz = [nin] + nouts
+        self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+    
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
+
+
+
+n = MLP(3, [4, 4, 1])
+        
+# draw_dot(n(x)).render('MLP', format='svg', cleanup=True)     
+
+
+xs = [
+    [2.0, 3.0, -1.0],
+    [3.0, -1.0, 0.5],
+    [0.5, 1.0, 1.0],
+    [1.0, 1.0, -1.0]
+]
+ys = [1.0, -1.0, -1.0, 1.0]
+
+for k in range(20):
+    ypred = [n(x) for x in xs]
+    loss = sum((yout-ygt)**2 for ygt, yout in zip(ys, ypred) )
+
+    for p in n.parameters():
+        p.grad = 0.0
+        
+    loss.backward()
+
+    for p in n.parameters():
+        p.data += -0.05 * p.grad
+    
+    print(k, loss.data, )
+
+print(ypred)    
+
