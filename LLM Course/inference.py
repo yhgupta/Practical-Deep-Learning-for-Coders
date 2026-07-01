578531bc998e117ccafe83a82ea2767f470cff63
Yash Gupta <yhgupta0408@gmail.com>
2026-07-01 22:25:30 +0530

    Changes in inference.py

--- LLM Course/inference.py
+++ LLM Course/inference.py
@@ -1,8 +1,8 @@
-prompt = "I want to become extremely successful person."
+prompt = "I am an astronaut."
 
 from transformers import pipeline
 
-generator = pipeline("text-generation",  model="LLM Course/my_awesome_eli5_clm-model")
+generator = pipeline("text-generation",  model="./my_awesome_eli5_clm-model/checkpoint-3912")
 result = generator(prompt)
 
 print(result)
@@ -11,12 +11,12 @@ print(result)
 
 from transformers import AutoTokenizer
 
-tokenizer = AutoTokenizer.from_pretrained("LLM Course/my_awesome_eli5_clm-model")
+tokenizer = AutoTokenizer.from_pretrained("./my_awesome_eli5_clm-model/checkpoint-3912")
 inputs = tokenizer(prompt, return_tensors="pt").input_ids
 
 from transformers import AutoModelForCausalLM
 
-model = AutoModelForCausalLM.from_pretrained("LLM Course/my_awesome_eli5_clm-model")
+model = AutoModelForCausalLM.from_pretrained("./my_awesome_eli5_clm-model/checkpoint-3912")
 outputs = model.generate(inputs, max_new_tokens=100, do_sample=True, top_k=50, top_p=0.95)
 
 result_2 = tokenizer.batch_decode(outputs, skip_special_tokens=True)
